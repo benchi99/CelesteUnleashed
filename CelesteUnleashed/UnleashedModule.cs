df6bb3ca413e2eba1fe40d0056bc50b48ba35ba7
Rubén Bermejo Romero <mejoruben@gmail.com>
2021-11-14 23:36:30 +0100

    Initial commit

---
+++ CelesteUnleashed/UnleashedModule.cs
@@ -0,0 +1,59 @@
+﻿using Celeste;
+using Celeste.Mod;
+using System;
+
+namespace CelesteUnleashed
+{
+    public class UnleashedModule : EverestModule
+    {
+        public static UnleashedModule Instance;
+        private readonly static Random Random = new Random();
+
+        private readonly static string[] JUMP_SFX =
+        {
+            "event:/benchi99/jump1",
+            "event:/benchi99/jump2",
+            "event:/benchi99/jump3"
+        };
+
+        private readonly static string[] DASH_SFX =
+        {
+            "event:/benchi99/dash1",
+            "event:/benchi99/dash2"
+        };
+
+        public UnleashedModule()
+        {
+            Instance = this;
+        }
+
+        public override void Load()
+        {
+            On.Celeste.Player.Jump += OnPlayerJump;
+            On.Celeste.Player.DashBegin += OnDashBegin;
+        }
+
+        public override void Unload()
+        {
+            On.Celeste.Player.Jump -= OnPlayerJump;
+            On.Celeste.Player.DashBegin -= OnDashBegin;
+        }
+
+        private void OnPlayerJump(On.Celeste.Player.orig_Jump orig, Celeste.Player self, bool particles, bool playSfx)
+        {
+            Audio.Play(GetRandomAudioID(JUMP_SFX));
+            orig(self, particles, false);
+        }
+
+        private void OnDashBegin(On.Celeste.Player.orig_DashBegin orig, Player self)
+        {
+            Audio.Play(GetRandomAudioID(DASH_SFX));
+            orig(self);
+        }
+
+        private static string GetRandomAudioID(string[] audioIDs)
+        {
+            return audioIDs[Random.Next(0, audioIDs.Length)];
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Celeste Unleashed
+
+Feeling good!
+
+## Downloads
+
+- [Gamebanana](https://gamebanana.com/mods/335986) - 1-CLICK Install via Olympus available!
+- Check out the releases section in this repository.
+
+## Building the project locally
+
+You may require to set up Visual Studio with the [components required](https://cdn.discordapp.com/attachments/445236692136230943/698269484653215844/celeste.vsconfig) for developing Celeste mods with Everest, as well as [setting up the project](https://github.com/EverestAPI/Resources/wiki/Your-First-Code-Mod#project-setup) accordingly for building.
+
+This project is missing the FMOD bank built from the Celeste FMOD project, as well as the sound effects; both things not being my property at all, and thus being excluded from this GIT repository.
+
+## Contributing
+
+I mean do you _really_ want to contribute to this? Up to you, though, shoot a PR if you want.
